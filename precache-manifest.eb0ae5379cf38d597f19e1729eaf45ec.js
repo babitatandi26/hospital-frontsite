0ff32ac45b11d97ac3912575b70b0fe990a37c2d
babitatandi26 <babitatandi266@gmail.com>
2020-10-09 16:24:48 +0530

    Updates

---
+++ precache-manifest.eb0ae5379cf38d597f19e1729eaf45ec.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bbc85309d7ea6cc78b226a683d83b7a",
+    "url": "/hospital-frontsite/index.html"
+  },
+  {
+    "revision": "818bfc05b43ffab65304",
+    "url": "/hospital-frontsite/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "1924966a0604844a4047",
+    "url": "/hospital-frontsite/static/css/main.2e555c26.chunk.css"
+  },
+  {
+    "revision": "818bfc05b43ffab65304",
+    "url": "/hospital-frontsite/static/js/2.dc2b95ad.chunk.js"
+  },
+  {
+    "revision": "59b36dad69bc9e169a3b0e6df948f016",
+    "url": "/hospital-frontsite/static/js/2.dc2b95ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1924966a0604844a4047",
+    "url": "/hospital-frontsite/static/js/main.452a4fc6.chunk.js"
+  },
+  {
+    "revision": "6be67d53e923e05d3592",
+    "url": "/hospital-frontsite/static/js/runtime-main.a7f037e9.js"
+  },
+  {
+    "revision": "f78fbc23b8ea8173361b7b8c8e1d15dc",
+    "url": "/hospital-frontsite/static/media/001.f78fbc23.jpg"
+  },
+  {
+    "revision": "d97b0bcba66b0c13f4770b51a6c00042",
+    "url": "/hospital-frontsite/static/media/002.d97b0bcb.jpg"
+  },
+  {
+    "revision": "b9571eedf0c6a862f595b177ada699cb",
+    "url": "/hospital-frontsite/static/media/003.b9571eed.jpg"
+  },
+  {
+    "revision": "c9107ebb2a3b296d4b0fe988279290fc",
+    "url": "/hospital-frontsite/static/media/004.c9107ebb.jpg"
+  },
+  {
+    "revision": "169aeb760827e3db1092fabf2b91b80a",
+    "url": "/hospital-frontsite/static/media/005.169aeb76.jpg"
+  },
+  {
+    "revision": "2af3cbe6e252fab5d7b79aea5b21e7b2",
+    "url": "/hospital-frontsite/static/media/006.2af3cbe6.jpg"
+  },
+  {
+    "revision": "e8bed81e777aa7243191fef9e3874ca5",
+    "url": "/hospital-frontsite/static/media/007.e8bed81e.jpg"
+  },
+  {
+    "revision": "9ae8318c23ce04ee7de2c1edfdb337b3",
+    "url": "/hospital-frontsite/static/media/008.9ae8318c.jpg"
+  },
+  {
+    "revision": "4fd5f6813500e61f26b61cfc763f9c91",
+    "url": "/hospital-frontsite/static/media/009.4fd5f681.jpg"
+  },
+  {
+    "revision": "a875fa890482beec0e15c8060bbeaf62",
+    "url": "/hospital-frontsite/static/media/01.a875fa89.jpg"
+  },
+  {
+    "revision": "d9c2b8f2ba614344074e46607398efdc",
+    "url": "/hospital-frontsite/static/media/010.d9c2b8f2.jpg"
+  },
+  {
+    "revision": "f1489d93188606d9dff8502193fc6581",
+    "url": "/hospital-frontsite/static/media/011.f1489d93.jpg"
+  },
+  {
+    "revision": "03b490084b7484feaf97a0299aa22800",
+    "url": "/hospital-frontsite/static/media/012.03b49008.jpg"
+  },
+  {
+    "revision": "c0bdb8cd2806db58aad0020376c0e3b5",
+    "url": "/hospital-frontsite/static/media/02.c0bdb8cd.jpg"
+  },
+  {
+    "revision": "529abd4627921def5d3544693580d96b",
+    "url": "/hospital-frontsite/static/media/03.529abd46.jpg"
+  },
+  {
+    "revision": "285b5f45a63eb47c3e46efd930828f06",
+    "url": "/hospital-frontsite/static/media/04.285b5f45.jpg"
+  },
+  {
+    "revision": "aa767b6828054b36f40f9d062f768d57",
+    "url": "/hospital-frontsite/static/media/05a.aa767b68.jpg"
+  },
+  {
+    "revision": "ad77e41048afd4ed49138adf15c75d26",
+    "url": "/hospital-frontsite/static/media/07.ad77e410.jpg"
+  },
+  {
+    "revision": "ed049b7c936384d4f8ab8e1a607adb1b",
+    "url": "/hospital-frontsite/static/media/1.ed049b7c.jpg"
+  },
+  {
+    "revision": "6fedb3ae045ac032f13ade14358026e5",
+    "url": "/hospital-frontsite/static/media/2.6fedb3ae.jpg"
+  },
+  {
+    "revision": "a2933137cdf108c6c943bfe44fe7ab65",
+    "url": "/hospital-frontsite/static/media/3.a2933137.jpg"
+  },
+  {
+    "revision": "eb2879f2bf94181fa3fc5f07ad79ef6d",
+    "url": "/hospital-frontsite/static/media/4.eb2879f2.jpg"
+  },
+  {
+    "revision": "b8b479d4b704d46e890d3d531373486c",
+    "url": "/hospital-frontsite/static/media/logo2.b8b479d4.png"
+  },
+  {
+    "revision": "c6eb3fd7b2968cabd52effb0354b9b4f",
+    "url": "/hospital-frontsite/static/media/pexels-jonathan-borba-3259629(1).c6eb3fd7.jpg"
+  }
+]);
